1f336d7b36068d076e940432d3d3f2ff5b250372
degluttiat <alena.liahov@gmail.com>
2019-11-13 20:12:43 +0200

    Fixed 2

--- app/src/main/java/com/alenal/jd/FragmentRecyclerView.java
+++ app/src/main/java/com/alenal/jd/FragmentRecyclerView.java
@@ -15,7 +15,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 
-import com.alenal.jd.R;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Description;
