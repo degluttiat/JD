92a31b4e3077991e237c33e3f527b9deb941934d
degluttiat <alena.liahov@gmail.com>
2020-09-20 18:29:26 +0300

    Update Gradle and Libraries

--- app/src/main/java/com/alenal/jd/FragmentAdapter.java
+++ app/src/main/java/com/alenal/jd/FragmentAdapter.java
@@ -1,8 +1,8 @@
 package com.alenal.jd;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 
 public class FragmentAdapter extends FragmentPagerAdapter {
     private String tabTitles[] = new String[]{"About", "Plans"};
